dd66d8a74b5485f723831fbd8ec0120cdde1d1c5
Daniel Arce <alarto96@gmail.com>
2024-03-22 14:44:28 -0600

    recordcontrollerss positioned directly in controllers

--- routes/chronicDesease.js
+++ routes/chronicDesease.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const chronicDeseaseRecordController = require("../controllers/recordForms/chronicDesease");
+const chronicDeseaseRecordController = require("../controllers/chronicDesease");
 const auth = require("../middlewares/auth");
 
 router.post("/create",[auth.authToken], chronicDeseaseRecordController.createChronicDeseaseRecord);
